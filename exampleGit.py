c1ef67d69de12660431d574e146ebd3911859ac5
tiacamp <tiacampagna@Tias-MacBook-Pro.local>
2021-03-25 12:17:40 -0400

    Example of GitHub

---
+++ exampleGit.py
@@ -0,0 +1,12 @@
+'''
+DS2500
+Spring 2021
+Final Project -- Spotify User Data
+exampleGit.py
+'''
+
+
+def main():
+    print('This is an example of how to use git hub')
+
+main()
---
+++ titlePolarity.py
@@ -0,0 +1,6 @@
+'''
+DS2500
+Spring 2021
+Final Project -- Spotify User Data
+titlePolarity.py
+'''
